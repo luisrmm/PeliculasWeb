fe1b4b07161b150466ff14c34d503603f23aa6de
luisrmm <luisrm5142@gmail.com>
2022-06-23 14:50:04 -0600

    U: change directory of components and pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { DetailsMoviesComponent } from './pages/details-movies/details-movies.component';
+import { HomeMoviesComponent } from './pages/home-movies/home-movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    SignInComponent,
+    DetailsMoviesComponent,
+    HomeMoviesComponent
   ],
   imports: [
     BrowserModule,
